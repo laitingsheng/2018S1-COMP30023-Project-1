c9e33e32da42d2cd4fd9fe82cff7dcaa8d786f4f
Tinson Lai <laitinson@gmail.com>
2018-04-15 23:08:18 +1000

    minor update

--- src/core/default.h
+++ src/core/default.h
@@ -4,12 +4,6 @@
 #define PORT 80
 #define PATH "/home/www"
 
-/*
- * for struct alignment, only allow path with a maximum length of 2046, as in
- * the httpd.c
- */
-#define PATH_MAX_LENGTH 2046
-
 #define SERVER_STRING "Simple HTTP Server - Project @ University of Melbourne"
 
 #define BUFFER_SIZE 4096
